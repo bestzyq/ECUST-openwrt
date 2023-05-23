0e8970fd35a216a6d228978bf0686468a9ab7ad2
chinazyq <2054555+chinazyq@user.noreply.gitee.com>
2023-05-23 13:44:20 +0000

    update install.sh. 国内替换
    Signed-off-by: chinazyq <2054555+chinazyq@user.noreply.gitee.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ opkg update
 opkg install luci-app-mwan3
 
 # 下载配置文件
-wget -O /etc/config/mwan3 https://raw.githubusercontent.com/chinazyq123/ECUST-openwrt/main/mwan3
+wget -O /etc/config/mwan3 https://gitee.com/chinazyq/ECUST-openwrt/raw/master/mwan3
 
 # 重新启动mwan3服务
 /etc/init.d/mwan3 restart
