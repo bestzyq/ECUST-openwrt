1438e7da3351ed56f4c938a3537e5be04d2b15eb
chinazyq <2054555+chinazyq@user.noreply.gitee.com>
2023-05-23 16:49:05 +0000

    增加选项
    Signed-off-by: chinazyq <2054555+chinazyq@user.noreply.gitee.com>

---
+++ install_onlyschool.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 更新软件包列表
+opkg update
+
+# 安装luci-app-mwan3
+opkg install luci-app-mwan3
+
+# 下载配置文件
+wget -O /etc/config/mwan3 https://gitee.com/chinazyq/ECUST-openwrt/raw/master/mwan3_onlyschool
+
+# 重新启动mwan3服务
+/etc/init.d/mwan3 restart
